6a0293c4c4e22cd3f0bb33b38e50ad53240411b1
Ming <gunmingfai@gmail.com>
2023-05-02 23:01:53 +0800

    ✅Carousell ✨Slightly fixed font sizes to be responsive 🚧Categories done Layout but Hover animation on the way

--- src/components/Carousell.tsx
+++ src/components/Carousell.tsx
@@ -1,5 +1,7 @@
 import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useCallback, useEffect, useState } from "react";
+import classNames from "classnames";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 // Define the props
 type Props = PropsWithChildren & EmblaOptionsType;
@@ -7,16 +9,74 @@ type Props = PropsWithChildren & EmblaOptionsType;
 const Carousel = ({ children, ...options }: Props) => {
   // 1. useEmblaCarousel returns a emblaRef and we must attach the ref to a container.
   // EmblaCarousel will use that ref as basis for swipe and other functionality.
-  const [emblaRef] = useEmblaCarousel(options);
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false)
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false)
+
+  const scrollNext = useCallback(() => {
+    emblaApi && emblaApi.scrollNext()
+  }, [emblaApi])
+
+  const scrollPrev = useCallback(() => {
+    emblaApi && emblaApi.scrollPrev()
+  }, [emblaApi])
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return
+    setPrevBtnEnabled(emblaApi.canScrollPrev())
+    setNextBtnEnabled(emblaApi.canScrollNext())
+  }, [emblaApi])
+
+  useEffect(() => {
+    if (!emblaApi) return
+    onSelect()
+    emblaApi.on('select', onSelect)
+    emblaApi.on('reInit', onSelect)
+  }, [emblaApi, onSelect])
 
   return (
-    // Attach ref to a div
-    // 2. The wrapper div must have overflow:hidden
-    <div className="overflow-hidden h-full flex items-center" ref={emblaRef}>
-      {/* 3. The inner div must have a display:flex property */}
-      {/* 4. We pass the children as-is so that the outside component can style it accordingly */}
-      <div className="flex">{children}</div>
+    <div className="flex items-center">
+      {/* // Attach ref to a div
+      // 2. The wrapper div must have overflow:hidden */}
+      <div className="mr-8">
+        <button
+          onClick={() => {
+            if (prevBtnEnabled) {
+              scrollPrev();
+            }
+          }}
+          disabled={!prevBtnEnabled}
+          className={classNames({
+            "text-gray-500": !prevBtnEnabled,
+            "text-black": prevBtnEnabled,
+          })}
+        >
+          <ChevronLeftIcon className='w-10 ml-1' ></ChevronLeftIcon>
+        </button>
+      </div>
+      <div className="overflow-hidden w-full h-full flex items-center px-3" ref={emblaRef}>
+        {/* 3. The inner div must have a display:flex property */}
+        {/* 4. We pass the children as-is so that the outside component can style it accordingly */}
+        <div className="flex">{children}</div>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (nextBtnEnabled) {
+              scrollNext();
+            }
+          }}
+          disabled={!nextBtnEnabled}
+          className={classNames({
+            "text-gray-500": !nextBtnEnabled,
+            "text-black": nextBtnEnabled,
+          })}
+        >
+          <ChevronRightIcon className='w-10 ml-1' ></ChevronRightIcon>
+        </button>
+      </div>
     </div>
+
   );
 };
 export default Carousel;
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,23 +1,32 @@
 import ProductCard from "@/components/ProductCard";
 import SearchInput from "@/components/SearchInput";
 import smartphone from './../assets/smartphone 1.png';
+import smartphoneCategory from './../assets/smartphone.jpg';
+import musicCategory from './../assets/music.jpg';
+import allCategory from './../assets/all.jpg';
+import homeCategory from './../assets/home.jpg';
+import computerCategory from './../assets/computers.jpg';
+import smartphoneCategoryBW from './../assets/smartphone_b&w.png';
+import musicCategoryBW from './../assets/music_b&w.png';
+import allCategoryBW from './../assets/all_b&w.png';
+import homeCategoryBW from './../assets/home_b&w.png';
+import computerCategoryBW from './../assets/computers_b&w.png';
 import Category from "@/enums/category_enum";
-import Product from "@/interfaces/product_interfaces";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-import { ReactElement} from "react";
+import { ReactElement } from "react";
 import Carousel from "./Carousell";
 
 const page1 = (
     <div className='flex h-[calc(100vh_-_5rem)]'>
         <div className='flex flex-col justify-center w-1/2 pl-40 mb-16'>
-            <div className='text-7xl'>
+            <div className='text-4xl lg:text-6xl xl:text-7xl'>
                 <span>Welcome to &nbsp;</span><span className='text-main font-bold -ml-4'>WeView</span>
             </div>
-            <div className='font-light text-3xl mt-3'>
+            <div className='font-light text-xl lg:text-2xl xl:text-3xl mt-3'>
                 <span>Your one-stop review platform for all tech products and electronics</span>
             </div>
-            <div className='mt-3 w-1/2'>
+            <div className='mt-3 w-full lg:w-2/3 xl:w-1/2'>
                 <SearchInput></SearchInput>
             </div>
 
@@ -33,59 +42,86 @@ const page1 = (
 )
 
 const trendingProducts: ReactElement[] = [];
-const testProduct: Product = {
-    name: 'iPhone 14 Pro',
-    type: Category.Smartphones,
-    rating: 0
+const testProduct = (index: number) => {
+    return {
+        name: 'iPhone 14 Pro ' + index,
+        type: Category.Smartphones,
+        rating: 0
+    }
 }
 
-for (let index = 0; index < 4; index++) {
-    trendingProducts.push(<div className="w-64 h-[19rem] mr-10">
-        <ProductCard product={testProduct}></ProductCard>
-        </div>)
+for (let index = 0; index < 10; index++) {
+    trendingProducts.push(<div className="w-[calc(100vw_*_20/100)] xl:w-[calc(100vw_*_15/100)]  h-[calc(100vw_*_20/100)] mx-10 p-3" key={index}>
+        <ProductCard product={testProduct(index)}></ProductCard>
+    </div>)
 
 }
 
 const page2 = (
-    <div className='flex flex-col h-screen items-center m-auto pt-14 w-4/5'>
+    <div className='flex flex-col items-center m-auto py-24 w-5/6'>
         <div className='flex flex-col w-full items-center'>
             <span className='text-5xl'>Trending</span>
             <span className='text-xl mt-5 text-black/60'>Find out what's the recent buzz</span>
         </div>
 
-        <div className='flex flex-col justify-center w-full h-3/5 pt-8'>
-            <Carousel>
-            {trendingProducts}
+        <div className='flex flex-col justify-center w-full h-3/5 pt-16'>
+            <Carousel align='start' slidesToScroll={4}>
+                {trendingProducts}
             </Carousel>
         </div>
-        <div className='flex pt-7 w-full justify-end'>
-            <div className='cursor-pointer flex'>
-                <span>View All</span> <ChevronRightIcon className='w-4 ml-1' ></ChevronRightIcon>
+        <div className='flex w-full justify-end mr-16 pt-12'>
+            <div className='cursor-pointer flex text-xl'>
+                <span>View All</span> <ChevronRightIcon className='w-5 ml-2' ></ChevronRightIcon>
             </div>
-
         </div>
 
     </div>
 )
 
 const page3 = (
-    <div className='flex flex-col h-full'>
-        <div className='flex flex-col w-full items-center mt-8'>
-            <span className='text-6xl'>Trending</span>
-            <span className='text-3xl mt-5 text-black/60'>Find out what's the recent buzz</span>
+    <div className='flex flex-col w-5/6 items-center m-auto h-screen'>
+        <div className='flex flex-col w-full items-center'>
+            <span className='text-5xl'>Categories</span>
         </div>
+        <div className="mt-16 w-full flex h-4/5">
+            <div className=" m-2 w-1/3 relative"
+                onMouseEnter={() => { }}
+                onMouseLeave={() => { }}>
+                <Image src={smartphoneCategory} alt="Smartphone Image" fill className="object-cover" />
+                <span className="text-main text-3xl font-medium absolute bottom-5 right-5 ">SMARTPHONES</span>
+            </div>
 
-        <div className='flex pt-7 justify-center'>
-            {trendingProducts}
+            <div className="flex flex-col basis-2/3 h-full">
+                <div className="m-2 flex h-1/2">
+                    <div className="bg-red-500 basis-2/5 mr-4 relative">
+                        <Image src={musicCategory} alt="Music Image" fill className="object-cover" />
+                        <span className="text-main text-3xl font-medium absolute bottom-5 right-5 ">MUSIC</span>
+                    </div>
+                    <div className="bg-yellow-500 basis-3/5 relative">
+                        <Image src={computerCategory} alt="Computer Image" fill className="object-cover" />
+                        <span className="text-main text-3xl font-medium absolute bottom-5 right-5 ">COMPUTER</span>
+                    </div>
+                </div>
+                <div className="m-2 flex h-1/2">
+                    <div className="bg-red-500 basis-3/5 mr-4 relative">
+                        <Image src={homeCategory} alt="Home Appliances Image" fill className="object-cover" />
+                        <span className="text-main text-3xl font-medium absolute bottom-5 right-5 ">HOME APPLIANCES</span>
+                    </div>
+                    <div className="basis-2/5 relative">
+                        <Image src={allCategory} alt="All Categories Image" fill className="object-cover" />
+                        <span className="text-main text-3xl font-medium absolute bottom-5 right-5 ">ALL</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
     </div>
 )
 
 const HomePage = () => {
 
     return (
-        <div className=''>
+        <div className='w-screen'>
             <div className='bg-white'>
                 {page1}
             </div>
